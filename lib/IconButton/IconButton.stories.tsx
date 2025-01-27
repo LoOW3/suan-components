1602b7368b259d68688bed16a72a662abc1eaa5e
LoOW3 <ignaciodiaznanni@gmail.com>
2025-01-27 03:35:13 -0300

    iconButton g-1.1

---
+++ lib/IconButton/IconButton.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { IconButton } from "./IconButton";
+
+const meta: Meta<typeof IconButton> = {
+  title: "Components/IconButton",
+  component: IconButton,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Chat: Story = {
+  args: {
+    iconColor: "green",
+  },
+};
+
+export const Upload: Story = {
+  args: {
+    iconColor: "white",
+    iconType: "upload",
+  },
+};
+
+export const Save: Story = {
+  args: {
+    iconColor: "white",
+    iconType: "save",
+  },
+};
---
+++ lib/IconButton/IconButton.tsx
@@ -0,0 +1,58 @@
+import ChatIcon from "@/icons/ChatIcon";
+import SaveIcon from "@/icons/SaveIcon";
+import UploadIcon from "@/icons/UploadIcon";
+import { cva, VariantProps } from "class-variance-authority";
+import { ComponentProps, forwardRef } from "react";
+import { cn } from "../utils";
+
+const iconMap = {
+  chat: ChatIcon,
+  save: SaveIcon,
+  upload: UploadIcon,
+};
+const iconButtonStyles = cva(
+  [
+    "w-full",
+    "rounded-xl",
+    "rounded-tl-none",
+    "font-semibold",
+    "focus:outline-none",
+    "disabled:cursor-not-allowed",
+    "bg-black",
+    "p-2",
+  ],
+  {
+    variants: {
+      size: {
+        sm: "scale-75",
+        md: "",
+        lg: "scale-125",
+      },
+    },
+    defaultVariants: {
+      size: "md",
+    },
+  }
+);
+
+type IconButtonProps = ComponentProps<"button"> &
+  VariantProps<typeof iconButtonStyles> & {
+    iconColor: "white" | "green";
+    iconType: "chat" | "save" | "upload";
+  };
+
+export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
+  ({ size, className, iconColor, iconType = "chat", ...props }, ref) => {
+    const Icon = iconMap[iconType];
+
+    return (
+      <button
+        ref={ref}
+        className={cn(iconButtonStyles({ size, className }))}
+        {...props}
+      >
+        <Icon size={18} color={iconColor === "white" ? "#FFF" : "#D7FE05"} />
+      </button>
+    );
+  }
+);
